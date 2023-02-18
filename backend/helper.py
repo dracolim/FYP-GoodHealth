761ac797120efaf9703022626b25b797268367f0
yvette1 <yvettewong25@gmail.com>
2023-02-18 09:42:37 +0800

    added remaining tables

--- backend/helper.py
+++ backend/helper.py
@@ -149,8 +149,130 @@ def getEducationRows(education):
             rows.append(i_row)
     return " ".join(rows)
 
+def getPresentationRows(presentation):
+    rows=[]
+    for i in presentation:
+        if i.Type!='Teaching' :
+            i_row = """<tr id="regtable">
+                <td id="regtable">
+                    <p style="text-align: center;">""" + str(i.id) + """</p>
+                </td>
+                <td id="regtable">
+                    <p style="text-align: center;">""" + i.Title + """</p>
+                </td>
+                <td id="regtable">
+                    <p style="text-align: center;">""" + i.Country + """</p>
+                </td>
+                <td id="regtable">
+                    <p style="text-align: center;">""" + i.Conference_Name + """</p>
+                </td>
+                <td id="regtable">
+                    <p style="text-align: center;">""" + i.Presentation_Date + """</p>
+                </td>
+            </tr>
+            """
+            rows.append(i_row)
+    if rows!=[]:
+        return " ".join(rows)
+    else:
+        return "<i>No Records Found</i>"
+
+def getTeachingPresentationRows(presentation):
+    rows=[]
+
+    for i in presentation:
+        if i.Type=='Teaching' :
+            i_row = """"<tr id="regtable">
+                <td id="regtable">
+                    <p >""" + i.Title + """</p>
+                </td>
+                <td id="regtable">
+                    <p style="text-align: center;">""" + i.Presentation_Date + """</p>
+                </td>
+            </tr>
+            """
+            rows.append(i_row)
+    if rows!=[]:
+        return " ".join(rows)
+    else:
+        return "<i>No Records Found</i>"
+    
+def getPublications(publications):
+    rows=[]
+
+    for i in publications:
+        i_row = """<tr id="regtable">
+            <td id="regtable">
+                <p style="text-align: center;">""" + i.PMID + """</p>
+            </td>
+            <td id="regtable">
+                <p style="text-align: center;">""" + i.Publication_Title + """</p>
+            </td>
+        </tr>
+        """
+        rows.append(i_row)
+    if rows!=[]:
+        return " ".join(rows)
+    else:
+        return "<i>No Records Found</i>"
+
+def getQIPatientSafetyRows(projects):
+    rows=[]
+
+    for i in projects:
+        i_row = """<tr id="regtable">
+            <td id="regtable">
+                <p style="text-align: center;">""" + i.Project_Title + """</p>
+            </td>
+            
+             <td id="regtable">
+                <p style="text-align: center;">""" + i.Start_Date + """</p>
+            </td>
+            <td id="regtable">
+                <p style="text-align: center;">""" + i.End_Date + """</p>
+            </td>"""
+        if i.Project_Type.lower()=='qi':
+            i_row+="""<td id="regtable">
+                <p style="text-align: center;"> X </p>
+            </td>
+             <td id="regtable">
+                <p style="text-align: center;"> </p>
+            </td>
+            <td id="regtable">
+                <p style="text-align: center;"> </p>
+            </td>"""
+        elif i.Project_Type.lower()=='patient safety': # check with singhealth
+            i_row+="""<td id="regtable">
+                <p style="text-align: center;">  </p>
+            </td>
+             <td id="regtable">
+                <p style="text-align: center;"> X </p>
+            </td>
+            <td id="regtable">
+                <p style="text-align: center;"> </p>
+            </td>"""
+        else:
+            i_row+="""<td id="regtable">
+                <p style="text-align: center;"> X </p>
+            </td>
+             <td id="regtable">
+                <p style="text-align: center;"> </p>
+            </td>
+            <td id="regtable">
+                <p style="text-align: center;">  </p>
+            </td>"""
+        i_row+="""</tr>"""
+        rows.append(i_row)
+    if rows!=[]:
+        return " ".join(rows)
+    else:
+        return "<i>No Records Found</i>"
+
+
 def getPage(name, mcrno, awardsRows, projectRows, educationalInvolvements, communityInvolvements,
-leadershipInvolvements, procedureLogsRows, postingRows, educationRows):
+leadershipInvolvements, procedureLogsRows, postingRows, educationRows,presentationRows,teachingPresentationRows,\
+    publicationsRows,patientSafetyQIRows):
+
     page = """<!DOCTYPE html>
     <html lang="en">
     <head>
@@ -476,6 +598,144 @@ leadershipInvolvements, procedureLogsRows, postingRows, educationRows):
         </tbody>
     </table>
 </div>
+
+<!-- Teaching Presentations SECTION: -->
+
+<p style="text-align: center; background-color: rgb(0, 0, 0); width:100%"><span style="color: rgb(255, 255, 255); background-color: rgb(0, 0, 0); width:100%">TEACHING PRESENTATIONS</span></p>
+
+<p><br></p>
+<p>Examples: To list Lecture, or presentation (such as grand rounds or case presentations) of at least 30 minute duration within the sponsoring institution or program. Includes teaching presentations in the various postings (Journal Club, Resident Rounds etc)</p>
+<p><br></p>
+<div align="left">
+    <table style="margin-right: calc(6%); width: 94%; border-color: black; width: 100%;border-collapse: collapse;">
+        <tbody id="regtable">
+            <tr id="regtable">
+                <td style="background-color: rgb(209, 213, 216); width:65%" id="regtable">
+                    <p style="text-align: center;">Title of Presentation</p>
+                </td>
+                <td style="background-color: rgb(209, 213, 216);" id="regtable">
+                    <p style="text-align: center;">Date</p>
+                </td>
+               
+            </tr>
+        
+           
+            """+ teachingPresentationRows + """
+            
+            
+        </tbody>
+    </table>
+</div>
+<!-- Other Presentations SECTION: -->
+
+<p style="text-align: center; background-color: rgb(0, 0, 0); width:100%"><span style="color: rgb(255, 255, 255); background-color: rgb(0, 0, 0); width:100%">ABSTRACT, POSTER & ORAL PRESENTATIONS</span></p>
+
+<p><br></p>
+<p>Includes Presentation given at international, national, or regional meetings</p>
+<p><br></p>
+<div align="left">
+    <table style="margin-right: calc(6%); width: 94%; border-color: black; width: 100%;border-collapse: collapse;">
+        <tbody id="regtable">
+            <tr id="regtable">
+                <td style="background-color: rgb(209, 213, 216); width:10%" id="regtable">
+                    <p style="text-align: center;">No.</p>
+                </td>
+                <td style="background-color: rgb(209, 213, 216);" id="regtable">
+                    <p style="text-align: center;">Name of Conference</p>
+                </td>
+                <td style="background-color: rgb(209, 213, 216);" id="regtable">
+                    <p style="text-align: center;">Country</p>
+                </td>
+                <td style="background-color: rgb(209, 213, 216);" id="regtable">
+                    <p style="text-align: center;">Title of Presentation</p>
+                </td>
+                <td style="background-color: rgb(209, 213, 216); width:20%" id="regtable">
+                    <p style="text-align: center;">Date of Presentation</p>
+                </td>
+            </tr>
+            """+ presentationRows + """
+            
+            
+        </tbody>
+    </table>
+</div>
+
+<!-- Publications SECTION: -->
+
+<p style="text-align: center; background-color: rgb(0, 0, 0); width:100%"><span style="color: rgb(255, 255, 255); background-color: rgb(0, 0, 0); width:100%">PUBLICATIONS (Articles)</span></p>
+
+<p><br></p>
+<p>Examples: To include articles that has been published. For articles published in PubMed, pls include PubMed ID (8 character numeric number). Publications not limited to PubMed.</p>
+<p><br></p>
+<div align="left">
+    <table style="margin-right: calc(6%); width: 94%; border-color: black; width: 100%;border-collapse: collapse;">
+        <tbody id="regtable">
+            <tr id="regtable">
+                <td style="background-color: rgb(209, 213, 216); width:20%" id="regtable">
+                    <p style="text-align: center;">PMID</p>
+                </td>
+                <td style="background-color: rgb(209, 213, 216);" id="regtable">
+                    <p style="text-align: center;">Details of Publication</p>
+                </td>
+                
+            </tr>
+            """+ publicationsRows + """
+            
+            
+        </tbody>
+    </table>
+</div>
+
+<!-- QI and Patient Safety SECTION: -->
+
+<p style="text-align: center; background-color: rgb(0, 0, 0); width:100%"><span style="color: rgb(255, 255, 255); background-color: rgb(0, 0, 0); width:100%">QUALITY IMPROVEMENT & PATIENT SAFETY PROJECTS</span></p>
+
+<p><br></p>
+<div align="left">
+    <table style="margin-right: calc(6%); width: 94%; border-color: black; width: 100%;border-collapse: collapse;">
+        <tbody id="regtable">
+            <tr id="regtable">
+            <td rowspan="2" style="background-color: rgb(209, 213, 216); width:30%" id="regtable">
+                <p style="text-align: center;">Project Title</p>
+            </td>
+            <td colspan="2" style="background-color: rgb(209, 213, 216);" id="regtable">
+                <p style="text-align: center;">QI Project Status</p>
+            </td>
+            <td colspan="3" style="background-color: rgb(209, 213, 216); width:15%" id="regtable">
+                <p style="text-align: center;">Please X the Appropriate Box</p>
+            </td>
+            
+        </tr>
+        <tr id="regtable">
+            <td  style="background-color: rgb(209, 213, 216); width:20%" id="regtable">
+                <p style="text-align: center;">Start Date</p>
+            </td>
+            <td  style="background-color: rgb(209, 213, 216); width:20%" id="regtable">
+                <p style="text-align: center;">End Date</p>
+            </td>
+            <td  style="background-color: rgb(209, 213, 216); width:5%" id="regtable">
+                <p style="text-align: center;">QI Project</p>
+            </td>
+            <td  style="background-color: rgb(209, 213, 216); width:5%" id="regtable">
+                <p style="text-align: center;">Patient Safety Project</p>
+            </td>
+            <td  style="background-color: rgb(209, 213, 216); width:5%" id="regtable">
+                <p style="text-align: center;">Both Qualified</p>
+            </td>
+          
+            
+        </tr>
+            """+ patientSafetyQIRows + """
+            
+            
+        </tbody>
+    </table>
+</div>
+
+
+
+
+
 
 </div>
 
