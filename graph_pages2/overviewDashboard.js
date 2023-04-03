596379e150b0709d4accf6049d7f87b7bada3d5c
yvette1 <yvettewong25@gmail.com>
2023-04-03 10:29:55 +0800

    combined Overview dashboard

--- graph_pages2/overviewDashboard.js
+++ graph_pages2/overviewDashboard.js
@@ -5,9 +5,10 @@ new Vue({
     return {
         year: 2023,
         mcr_no: "",
-        loaded: false,
+        // loaded: false,
 
 
+        ihiLoaded:false,
         IHIchartConfig: {
             labels: ["Completed (%)", "Not Completed (%)"],
             datasets: [
@@ -40,7 +41,7 @@ new Vue({
 
 
 
-
+            dutyLoaded:false,
             dutyChartConfig: {
                 labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
                 datasets: [
@@ -53,7 +54,7 @@ new Vue({
                     }
                 ]
                 },
-                dutyHourOptions: {
+            dutyHourOptions: {
                 responsive: true,
                 maintainAspectRatio: false,
                 title: {
@@ -93,23 +94,27 @@ new Vue({
 
 
 
-        scholarlyChartConfig: {
-            labels: ["% of Completion", "% of Non-Completioin"],
-            datasets: [
-                {
-                data: [],
-                backgroundColor: ['#ff6b00','#ffedd5'],
-                borderColor: 'rgba(136,136,136,0.5)',
-                label: ["Compliant", "Non-Compliant"]
-                }
-            ]
+            total: 0,
+            scholarlyPassed: 0,
+            scholarlyMissing: 0,
+            //scholarlyChartArray: [],
+            scholarlyLoaded: false,
+            mcr_no: null,
+            scholarlyChartConfig: {
+                labels: ["% of Completion", "% of Non-Completioin"],
+                datasets: [{
+                    data: [],
+                    backgroundColor: ['#ff6b00', '#ffedd5'],
+                    borderColor: 'rgba(136,136,136,0.5)',
+                    label: ["Compliant", "Non-Compliant"]
+                }]
             },
             scholarlyOptions: {
                 responsive: true,
                 maintainAspectRatio: false,
                 title: {
                     display: true,
-                    text: 'Scholarly Activity (Resident)'
+                    text: 'Scholarly Activity (Overview)'
                 },
                 tooltips: {
                     mode: 'index',
@@ -119,11 +124,13 @@ new Vue({
                     mode: 'nearest',
                     intersect: true
                 },
-                scales: {
-                }
+                scales: {}
             },
+            mcr_arr: [],
+            scholarlyChartData: "",
 
 
+            projectsLoaded:false,
             projectChartConfig: {
                 labels: ["Completed (%)", "Not Completed (%)"],
                 datasets: [
@@ -134,7 +141,7 @@ new Vue({
                     label: "Compliant"
                     }
                 ]
-            },
+                },
             projectOptions: {
                 responsive: true,
                 maintainAspectRatio: false,
@@ -152,11 +159,11 @@ new Vue({
                 },
                 scales: {
                 }
-            },
+                },
 
 
 
-
+            didacticLoaded:false,
             didacticChartConfig: {
                 labels: ["Jul-Dec", "Jan-Jun"],
                 datasets: [
@@ -209,13 +216,146 @@ new Vue({
     }
     },
     mounted: function () {
-        this.loaded = false
+        // this.loaded = false
         // this.getSpecificDidacticAttendanceData();
         this.getIhiData();
         this.getDutyHourLogData()
         this.getProjectsData()
+        this.getDidacticAttendanceData()
+        
+        specificURL = "http://localhost:5011/personaldetail"
+        axios.get(specificURL)
+            .then(response => {
+                this.total = response.data.data.length
+                for (i = 0; i < response.data.data.length; i++) {
+                    this.mcr_arr.push(response.data.data[i].MCR_No)
+                }
+                this.getScholarlyActivitiesData();
+            })
+            .catch(function (error) {
+                console.log(error);
+                this.scholarlyMissing += 1
+            });
         },
     methods: {
+        getScholarlyActivitiesData: async function () {
+            // get all residents
+            for (each of this.mcr_arr) {
+                console.log(this.getResidentData(each))
+                if (await this.getResidentData(each) == true) {
+                    this.scholarlyPassed += 1 // if compliant
+                }
+            }
+
+
+            // need to wait for the getResidentData to add to the this.scholarlyPassed first before calling the this.getScholarlyChartData
+            chartArray = [this.scholarlyPassed, this.total, this.missing]; //getScholarlyChartData will use to show visualisation
+
+            console.log('this is chartArray')
+            console.log(chartArray)
+
+            this.getScholarlyChartData(chartArray); // it works
+            this.scholarlyLoaded = true
+        },
+
+        getResidentData: async function (mcr) {
+            specificURL = "http://localhost:5011/profile/" + mcr
+            console.log(specificURL)
+            var toAdd = false;
+
+            await axios.get(specificURL).then((response) => {
+                    this.scholarlyChartData = response.data
+                })
+                .catch(function (error) {
+                    console.log(error);
+                    this.scholarlyMissing += 0
+                });
+            
+            // check compliance for resident
+            percentageCompletion = 0
+            count = 0
+            hasTeachingPresentation = false
+            hasAbstractPresentation = false
+            hasPublication = false
+            const d = new Date();
+            let year = d.getFullYear();
+            // var toAdd = false;
+
+            if (this.scholarlyChartData.data.publications.length > 0) {
+                publicationArray = this.scholarlyChartData.data.publications
+                for (let i = 0; i < publicationArray.length; i++) {
+                    year1 = parseInt(publicationArray[i]['Publication_Date'].slice(-2))
+                    if ((year1 == year - 2000) && hasPublication == false) {
+                        count += 1
+                        hasPublication = true
+                        // console.log('publication')
+                    }
+                }
+            }
+            if (this.scholarlyChartData.data.presentations.length > 0) {
+                presentationArray = this.scholarlyChartData.data.presentations
+                // console.log(presentationArray)
+                if (presentationArray.length == 1) {
+                    if (parseInt(presentationArray[0]['Presentation_Date'].slice(-4)) ==
+                        year) {
+                        count += 1
+
+                    }
+                } else {
+                    for (let i = 0; i < presentationArray.length; i++) {
+                        if (parseInt(presentationArray[i]['Presentation_Date'].slice(-
+                                4)) == year) {
+                            if (presentationArray[i]['Type'].length == 0 &&
+                                hasTeachingPresentation == false) {
+                                count += 1
+                                console.log('teaching')
+                                hasTeachingPresentation = true
+                            }
+                            if (presentationArray[i]['Type'].length > 0 &&
+                                hasAbstractPresentation == false) {
+                                count += 1
+                                hasAbstractPresentation = true
+                            }
+                            if (hasAbstractPresentation == true &&
+                                hasTeachingPresentation == true) {
+                                break
+                            }
+                        }
+                    }
+                }
+            }
+            if (count != 0) {
+                percentageCompletion = count / 3
+                // console.log('percentageCompletion1')
+                // console.log(percentageCompletion)
+            }
+            console.log('percentageCompletion2')
+            console.log(percentageCompletion)
+            if (count == 1) {
+                console.log('true')
+                toAdd = true;
+            }
+            return toAdd;
+        },
+
+        getScholarlyChartData: function (chartData) {
+            
+            passedResident = 0
+            totalResident = 0
+            percentageCompletion = 0.0
+            percentageNonCompletion = 0.0
+
+            passedResident = chartData[0]
+            totalResident = chartData[1]
+            percentageCompletion = passedResident / totalResident
+            percentageNonCompletion = 1 - percentageCompletion
+            this.scholarlyChartConfig.datasets[0].data.push(percentageCompletion.toFixed(3))
+            this.scholarlyChartConfig.datasets[0].data.push(percentageNonCompletion.toFixed(3))
+            
+        },
+
+
+
         getIhiData: async function () {
 
             this.status = "getting data...";
@@ -225,7 +365,7 @@ new Vue({
             // console.log(response.data)
 
             this.getIHIchartData(response.data);
-            this.loaded = true
+            this.ihiLoaded = true
             })
             .catch(function (error) {
                 console.log(error);
@@ -264,7 +404,7 @@ new Vue({
             await axios.get("http://localhost:5011/duty_hour_log").then((response) => {
             // console.log(response.data)
             this.getDutyHourChartData(response.data);
-            this.loaded = true
+            this.dutyLoaded = true
             })
             .catch(function (error) {
                 console.log(error);
@@ -470,6 +610,9 @@ new Vue({
 
             console.log(this.dutyChartConfig);
         },
+
+
+        
         getProjectsData: async function () {
 
             this.status = "getting data...";
@@ -479,7 +622,7 @@ new Vue({
             // console.log(response.data)
 
             this.getProjectChartData(response.data);
-            this.loaded = true
+            this.projectsLoaded = true
             })
             .catch(function (error) {
                 console.log(error);
@@ -523,11 +666,11 @@ new Vue({
 
             await axios.get("http://localhost:5011/didactic_attendance").then((response) => {
             // console.log("This is responseData")
-            // console.log(response.data)
-        
+            console.log(response.data,'===================')
+            
             this.getDidacticChartData(response);
-            this.loaded = false
-            this.loaded = true
+            // this.loaded = false
+            this.didacticLoaded = true
             })
             .catch(function (error) {
                 console.log(error);
