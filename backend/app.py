a771bb67878b9afb449a106fc9af062e3f43b56c
yvette1 <yvettewong25@gmail.com>
2023-06-06 01:06:18 +0800

    fixed

--- backend/app.py
+++ backend/app.py
@@ -3369,6 +3369,7 @@ def delete_presentation(id):
     db.session.commit()
     return 'presentation deleted', 200
 
+import io
 @app.route("/export_all_excel")
 def export_all_excel():
 
@@ -3377,74 +3378,91 @@ def export_all_excel():
 
     presentations = Presentations.query.all()
     presentations_df=pd.DataFrame([pd.to_dict() for pd in presentations])
-    presentations_df=presentations_df.drop('id',axis=1)
+    if not presentations_df.empty:
+        presentations_df=presentations_df.drop('id',axis=1)
 
     awards = Awards.query.all()
     awards_df=pd.DataFrame([pd.to_dict() for pd in awards])
-    awards_df=awards_df.drop('id',axis=1)
+    if not awards_df.empty:
+        awards_df=awards_df.drop('id',axis=1)
 
     projects = Projects.query.all()
     projects_df=pd.DataFrame([pd.to_dict() for pd in projects])
-    projects_df=projects_df.drop('id',axis=1)
+    if not projects_df.empty:
+        projects_df=projects_df.drop('id',axis=1)
 
     education = Education_History.query.all()
     education_df=pd.DataFrame([pd.to_dict() for pd in education])
-    education_df=education_df.drop('id',axis=1)
+    if not education_df.empty:
+        education_df=education_df.drop('id',axis=1)
 
     involvement = Involvement.query.all()
     involvement_df=pd.DataFrame([pd.to_dict() for pd in involvement])
-    involvement_df=involvement_df.drop('id',axis=1)
+    if not involvement_df.empty:
+        involvement_df=involvement_df.drop('id',axis=1)
 
     posting = Posting_History.query.all()
     posting_df=pd.DataFrame([pd.to_dict() for pd in posting])
-    posting_df=posting_df.drop('id',axis=1)
+    if not posting_df.empty:
+        posting_df=posting_df.drop('id',axis=1)
 
     publications = Publications.query.all()
     publications_df=pd.DataFrame([pd.to_dict() for pd in publications])
-    publications_df=publications_df.drop('id',axis=1)
+    if not publications_df.empty:
+        publications_df=publications_df.drop('id',axis=1)
 
     exam = Exam_History.query.all()
     exam_df=pd.DataFrame([pd.to_dict() for pd in exam])
-    exam_df=exam_df.drop('id',axis=1)
+    if not exam_df.empty:
+        exam_df=exam_df.drop('id',axis=1)
 
     ihi = IHI.query.all()
     ihi_df=pd.DataFrame([pd.to_dict() for pd in ihi])
-    ihi_df=ihi_df.drop('id',axis=1)
+    if not ihi_df.empty:
+        ihi_df=ihi_df.drop('id',axis=1)
 
     grants = Grants.query.all()
     grants_df=pd.DataFrame([pd.to_dict() for pd in grants])
-    grants_df=grants_df.drop('id',axis=1)
+    if not grants_df.empty:
+        grants_df=grants_df.drop('id',axis=1)
 
     trg = TrgExtRem_History.query.all()
     trg_df=pd.DataFrame([pd.to_dict() for pd in trg])
-    trg_df=trg_df.drop('id',axis=1)
+    if not trg_df.empty:
+        trg_df=trg_df.drop('id',axis=1)
 
     case_log = Case_Log.query.all()
     case_log_df=pd.DataFrame([pd.to_dict() for pd in case_log])
-    case_log_df=case_log_df.drop('id',axis=1)
+    if not case_log_df.empty:
+        case_log_df=case_log_df.drop('id',axis=1)
 
     didactic = Didactic_Attendance.query.all()
     didactic_df=pd.DataFrame([pd.to_dict() for pd in didactic])
-    didactic_df=didactic_df.drop('id',axis=1)
+    if not didactic_df.empty:
+        didactic_df=didactic_df.drop('id',axis=1)
 
     duty_hour = Duty_Hour_Log.query.all()
     duty_hour_df=pd.DataFrame([pd.to_dict() for pd in duty_hour])
-    duty_hour_df=duty_hour_df.drop('id',axis=1)
+    if not duty_hour_df.empty:
+        duty_hour_df=duty_hour_df.drop('id',axis=1)
 
     evaluations = Evaluations.query.all()
     evaluations_df=pd.DataFrame([pd.to_dict() for pd in evaluations])
-    evaluations_df=evaluations_df.drop('id',axis=1)
+    if not evaluations_df.empty:
+        evaluations_df=evaluations_df.drop('id',axis=1)
 
     procedures = Procedure_Log.query.all()
     procedures_df=pd.DataFrame([pd.to_dict() for pd in procedures])
-    procedures_df=procedures_df.drop('id',axis=1)
+    if not procedures_df.empty:
+        procedures_df=procedures_df.drop('id',axis=1)
 
-
-
-
-
+    time=datetime.now()
+    runtime=time.strftime('%Y-%m-%d-%H-%M-%S')
+    filename='excel_'+runtime+'.xlsx'
+    output = io.BytesIO()
     # create a excel writer object
-    with pd.ExcelWriter("path to file\fil2ename2.xlsx") as writer:
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        
         # use to_excel function and specify the sheet_name and index
         # to store the dataframe in specified sheet
         personal_details_df.to_excel(writer, sheet_name="Personal Details", index=False)
@@ -3459,18 +3477,13 @@ def export_all_excel():
         ihi_df.to_excel(writer, sheet_name="IHI", index=False)
         grants_df.to_excel(writer, sheet_name="Grants", index=False)
         trg_df.to_excel(writer, sheet_name="Trg", index=False)
-        case_log.to_excel(writer, sheet_name="Case Logs", index=False)
-        didactic.to_excel(writer, sheet_name="Didactic Attendance", index=False)
-        duty_hour.to_excel(writer, sheet_name="Duty Hour Logs", index=False)
-        evaluations.to_excel(writer, sheet_name="Evaluations", index=False)
+        case_log_df.to_excel(writer, sheet_name="Case Logs", index=False)
+        didactic_df.to_excel(writer, sheet_name="Didactic Attendance", index=False)
+        duty_hour_df.to_excel(writer, sheet_name="Duty Hour Logs", index=False)
+        evaluations_df.to_excel(writer, sheet_name="Evaluations", index=False)
         procedures_df.to_excel(writer, sheet_name="Procedure Logs", index=False)
-
-    return jsonify(
-        {
-            "data": [pd.to_dict()
-                     for pd in personal_details]
-        }
-    ), 200
+    output.seek(0)
+    return send_file(output, mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', as_attachment=True,download_name=filename)
 
 
 # ============================
